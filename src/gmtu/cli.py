35f9110b5ea194315a96265a677a7906e5b2ff50
txstc55 <txstc55@gmail.com>
2025-05-06 00:27:50 -0700

    fml i went all the way back

--- src/gmtu/cli.py
+++ src/gmtu/cli.py
@@ -5,9 +5,10 @@ def main():
   parser = argparse.ArgumentParser(description="Greet with gmtu class.")
   parser.add_argument("--content", type = str, default="There's an update", help="the push notification content")
   parser.add_argument("--fcm-token", type = str, default="unknown", help="the fcm token of the device you want to send to")
+  parser.add_argument("--silence", action="store_true", help="silence the push notification")
   args = parser.parse_args()
   instance = gmtu(fcm_token = args.fcm_token)
-  instance.sendOneTimeEvent(args.content)
+  instance.sendOneTimeEvent(args.content, silence=args.silence)
 
 if __name__ == "__main__":
   main()
--- src/gmtu/gmtu.py
+++ src/gmtu/gmtu.py
@@ -2,21 +2,69 @@ import socket
 import os
 import platform
 import subprocess
-import uuid
 import threading
 import requests
+import uuid
+import datetime
+from typing import List, Dict, Any
+
 
 class gmtu:
-  def __init__(self, supabase_url = "", supabase_anon_key = "", fcm_token = ""):
+  def __get_machine_uuid():
+    system = platform.system()
+    if system == "Linux":
+      try:
+        with open('/etc/machine-id', 'r') as f:
+          return f.read().strip()
+      except FileNotFoundError:
+        pass
+    elif system == "Darwin":  # macOS
+      try:
+        output = subprocess.check_output(
+          "ioreg -rd1 -c IOPlatformExpertDevice | grep IOPlatformUUID",
+          shell=True
+        ).decode()
+        return output.split('"')[-2]
+      except Exception:
+        pass
+
+    elif system == "Windows":
+      try:
+        output = subprocess.check_output(
+          "wmic csproduct get UUID",
+          shell=True
+        ).decode().split("\n")[1].strip()
+        return output
+      except Exception:
+        return "Unknown_System_ID"
+  ####################################################################################
+  ## Static variable and methods for if user want to configure things globally
+  ####################################################################################
+  __default_supabase_url = "https://yhrybsftocoyxlcntlvl.supabase.co/functions/v1/send-push"
+  __default_anon_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlocnlic2Z0b2NveXhsY250bHZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4MTg3MzEsImV4cCI6MjA2MTM5NDczMX0.8NND4xnb4DCs8QVgPfY3nxmHB1Lw4Z3XzZN9utOsMhE"
+
+  @classmethod
+  def set_default_supabase_url(cls, url):
+    cls.__default_supabase_url = url
+
+  @classmethod
+  def set_default_anon_key(cls, key):
+    cls.__default_anon_key = key
+
+  __system_name: str = socket.gethostname()
+  __system_id: str = __get_machine_uuid()
+
+
+  def __init__(self, supabase_url = "", supabase_anon_key = "", fcm_token = "", event_name = "Code Progress"):
     ####################################################################################
     ## First we check for supabase anon key and url
     ####################################################################################
     self.__supabase_url = supabase_url
     self.__supabase_anon_key = supabase_anon_key
-    if supabase_url == "":
+    if supabase_url == "" and supabase_anon_key == "":
       # we will use the default supabase edge function for sending the push notification
-      self.__supabase_url = "https://yhrybsftocoyxlcntlvl.supabase.co/functions/v1/send-push"
-      self.__supabase_anon_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlocnlic2Z0b2NveXhsY250bHZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4MTg3MzEsImV4cCI6MjA2MTM5NDczMX0.8NND4xnb4DCs8QVgPfY3nxmHB1Lw4Z3XzZN9utOsMhE"
+      self.__supabase_url = self.__class__.__default_supabase_url
+      self.__supabase_anon_key = self.__class__.__default_anon_key
 
     if supabase_url == "" and supabase_anon_key != "":
       raise ValueError(f"gmtu.__init__: supabase_url is empty but supabase_anon_key is not empty, anon_key is:{supabase_anon_key}")
@@ -31,10 +79,11 @@ class gmtu:
     ####################################################################################
     ## now we set the machine instance
     ####################################################################################
-    self.__system_uuid: str = "unknown_system_id"
-    self.__system_name: str = socket.gethostname()
-    self.__get_machine_uuid()
-    self.__events = {}
+    self.__system_uuid: str = self.__class__.__system_id
+    self.__system_name: str = self.__class__.__system_name
+    self.__milestones: Dict[float, str] = {}
+    self.__event_name: str = event_name
+    self.__event_uuid: str = uuid.uuid4()
     # print(f"GMTU initialized with name {self.__system_name} and UUID {self.__system_uuid}")
 
 
@@ -50,8 +99,8 @@ class gmtu:
     return self.__system_name
 
   @property
-  def events(self):
-    return self.__events
+  def event_name(self):
+    return self.__event_name
 
   @property
   def fcm_token(self):
@@ -61,11 +110,14 @@ class gmtu:
   def fcm_token(self, token):
     self.__fcm_token = token
 
+  def __get_id(self, event_name):
+    return str(uuid.uuid5(uuid.NAMESPACE_DNS, f"{self.__event_name}_{self.__system_name}_{event_name}_{datetime.datetime.utcnow().isoformat()}_{uuid.uuid4()}"))
+
 
   ####################################################################################
   ## the most important function, used to send a push notification
   ####################################################################################
-  def __sendPushNotification(self, type, event_name = "", milestones = [], progression = "", eventId = 0):
+  def __sendPushNotification(self, type, event_name = "", milestones = [], progression: float = 0, eventId: str = "0", silence: bool = False):
     # let's say
     # type 0 is an one time update, so no progression
     # type 1 is an event starting
@@ -86,9 +138,10 @@ class gmtu:
         "event_name": event_name,
         "milestones": milestones,
         "progression": progression,
-        "device_id": self.__system_uuid,
-        "device_name": self.__system_name,
-        "event_id": eventId
+        "device_id": self.system_uuid,
+        "device_name": self.system_name,
+        "event_id": eventId,
+        "silence": silence
       }
       try:
         response = requests.post(self.__supabase_url, headers=headers, json=data)
@@ -101,75 +154,80 @@ class gmtu:
 
 
   ####################################################################################
-  ## add event will need an event name and milestones
-  ## the milestones are used to determine the progress of the event
-  ## by default calling this function will immediately start the event
-  ####################################################################################
-  def addEvent(self, event_name: str, milestones = [], immediate_start = True) -> None:
-    if event_name in self.__events:
-      print(f"gmtu.addEvent: {event_name} already exists, this action will override the existing event. This is only a warning, it will not interrupt the existing execution")
-    if milestones != []:
-      self.__events[event_name] = milestones
-    else:
-      self.__events[event_name] = list(range(100)) # well this is from 0 to 99 but what can we do
-    if immediate_start:
-      self.startEvent(event_name)
-
-  ####################################################################################
-  ## this is basically for modifying the milestones
-  ####################################################################################
-  def addMilestones(self, event_name: str, milestones) -> None:
-    # this basically modify the existing event with the new milestones
-    if event_name in self.__events:
-      self.__events[event_name] = milestones
-
-  ####################################################################################
-  ## function to start the event
-  ####################################################################################
-  def startEvent(self, event_name: str) -> None:
-    self.__sendPushNotification(1, event_name, milestones = [])
-
-
-  ####################################################################################
   ## function to send a one time event notification
   ## this can be used in cli for like, hey compilation finished etc
   ####################################################################################
-  def sendOneTimeEvent(self, content) -> None:
-    self.__sendPushNotification(0, content) # here the event name field in the send notification function will be the content of the push notification
+  def sendOneTimeEvent(self, content, silence = False) -> None:
+    otu_event_id = self.__get_id("otu")
+    self.__sendPushNotification(0, content, eventId = otu_event_id, silence = silence) # here the event name field in the send notification function will be the content of the push notification
 
 
-  def updateEvent(self, event_name, milestone = "", single_update = False, title = "", content = "") -> None:
-    if self.__fcm_token is None:
-      raise ValueError("gmtu.sendEventUpdate: FCM token is not set")
-    if not single_update:
-      if event_name not in self.__events:
-        raise ValueError(f"gmtu.sendEventUpdate: Event {event_name} does not exist")
+  ####################################################################################
+  ## here we make a class that takes in an iterator
+  ####################################################################################
+  def setMilestones(self, milestones: Dict[float, str]) -> None:
+    if (len(milestones) > 10):
+      # send a warning that the number of milestones is too high
+      print('''
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!  gmtu.setMilestones Warning: The number of milestones is too high  !!!!!!!!!!
+!!!!!!!!!!  We recomment reducing the number of milestones                    !!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+''')
+    for key in milestones.keys():
+      if key <= 0:
+        raise ValueError(f'Milestone key {key} must be greater than 0')
+      elif key >= 1:
+        raise ValueError(f'Milestone key {key} must be less than 1')
+    self.__milestones = OrderedDict(sorted(milestones.items()))
 
+  def __call__(self, iterable, event_name = None):
+    # When instance is called with an iterable, return a wrapped iterator
+    if event_name is not None:
+      self.__event_name = event_name
+    self.__event_uuid = self.__get_id(self.__event_name) # we make a unique uuid for each event
+    return self.Iterator(iterable, self.__event_name, self, self.__milestones)
 
-  def __get_machine_uuid(self):
-    system = platform.system()
-    if system == "Linux":
+  class Iterator:
+    def __init__(self, iterable, event_name: str, parent, milestones: Dict[float, str]):
+      self.__iterable = iterable
+      self.__iterator = iter(iterable)
+      self.__event_name = event_name
+      self.__count = 0
+      self.__total = len(iterable) if hasattr(iterable, "__len__") else None
+      self.__parent = parent
+      self.__started = False
+      self.__milestones = milestones
+
+    def __iter__(self):
+      return self
+
+    def __next__(self):
+      if not self.__started:
+        self.__started = True
+        # send notification for event starting
+        # self.__parent._gmtu__sendPushNotification(1, self.__event_name, milestones = self.__milestones, eventId = self.__parent._gmtu__event_uuid)
+        print("Event started")
+
       try:
-        with open('/etc/machine-id', 'r') as f:
-          self.__system_uuid = f.read().strip()
-      except FileNotFoundError:
-        pass
-    elif system == "Darwin":  # macOS
-      try:
-        output = subprocess.check_output(
-          "ioreg -rd1 -c IOPlatformExpertDevice | grep IOPlatformUUID",
-          shell=True
-        ).decode()
-        self.__system_uuid = output.split('"')[-2]
-      except Exception:
-        pass
+        value = next(self.iterator)
+        self.count += 1
+        if self.total:
+          print(f"{self.name}: {self.count}/{self.total}")
+        else:
+          print(f"{self.name}: {self.count}")
+          return value
+      except StopIteration:
+        # → Loop ended normally here
+        # self.__parent._gmtu__sendPushNotification(3, self.__event_name, milestones = self.__milestones, eventId = self.__parent._gmtu__event_uuid)
+        print("Event ended")
+        raise
 
-    elif system == "Windows":
-      try:
-        output = subprocess.check_output(
-          "wmic csproduct get UUID",
-          shell=True
-        ).decode().split("\n")[1].strip()
-        self.__system_uuid = output
-      except Exception:
-        pass
+    def __del__(self):
+      if self.__started and self.count != self.total:
+        # self.__parent._gmtu__sendPushNotification(4, self.__event_name, milestones = self.__milestones, eventId = self.__parent._gmtu__event_uuid)
+        print("Event cancelled!")
+
+  ####################################################################################
+  ## helper functions
+  ####################################################################################

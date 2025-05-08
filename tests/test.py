395c7ce2207c4fd1cbc3838fdc77db4ee8d01b8d
txstc55 <txstc55@gmail.com>
2025-05-07 19:05:50 -0700

    update readme

--- tests/test.py
+++ tests/test.py
@@ -5,7 +5,16 @@ fcm_token = f.read().strip()
 
 gmtu_instance = gmtu(fcm_token)
 
-for i in gmtu_instance(range(10038), event_name = "python for loop testing"):
+for i in gmtu_instance(range(138), event_name = "for loop test 1"):
+  if i == 3:
+    time.sleep(0.1)
+    gmtu_instance.sendOTU("Message in between")
+    time.sleep(1)
+  time.sleep(0.2)
+  print(i)
+
+
+for i in gmtu_instance(range(123), event_name = "for loop test 2"):
   if i == 3:
     time.sleep(0.1)
     gmtu_instance.sendOTU("Message in between")
